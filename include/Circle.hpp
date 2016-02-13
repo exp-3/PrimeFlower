097b8e7caaf15d8e9c57a5f7adb140c183be306e
exp-3 <takaaki.service@gmail.com>
2016-02-13 13:22:01 +0900

    atom package test

--- include/Circle.hpp
+++ include/Circle.hpp
@@ -4,28 +4,30 @@
 #include <map>
 
 class Circle {
-public:
-    /** コンストラクタ*/
-    Circle();
+ public:
+  /** コンストラクタ*/
+  Circle();
 
-    /** 円の半径を設定する*/
-    void setScale(double tScale);
+  /** 円の半径を設定する*/
+  void setScale(double tScale);
 
-    /** 円の中心座標を設定する*/
-    void setPosition(double tX, double tY, double tZ);
+  /** 円の中心座標を設定する*/
+  void setPosition(double tX, double tY, double tZ);
 
-    /** 円を表示する*/
-    void display();
+  /** 円を表示する*/
+  void display();
 
-protected:
-    class Vertices : public std::multimap<double, double> {
-    public:
-        Vertices(int num);
-    };
+ protected:
+  class Vertices : public std::multimap<double, double> {
+   public:
+    Vertices(int num);
+  };
 
-    int countFromPositionSetting;
-    int countFromScaleSetting;
-    double srcX, srcY, srcZ, dstX, dstY, dstZ, srcScale, dstScale;
-    static int num;
-    static Vertices vertices;
+  int countFromPositionSetting;
+  int countFromScaleSetting;
+  double srcX, srcY, srcZ, dstX, dstY, dstZ, srcScale, dstScale;
+  static int num;
+  static Vertices vertices;
 };
+
+void test() { ; }
